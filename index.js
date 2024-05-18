afee17973906822bc735ac084e3fbb386438a82a
navnee8989 <sondarvanavneet444@gmail.com>
2024-05-18 18:03:08 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const userRoutes = require("./routes/Authroutes");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const cors = require("cors"); // Import cors package
+const cors = require("cors");
 
 dotenv.config();
 
@@ -12,14 +12,33 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // Enable CORS for all routes and all origins
-app.use(cors({ origin: '*' }));
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Origin",
+    "X-Requested-With",
+  ],
+  exposedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept",
+    "Origin",
+    "X-Requested-With",
+  ],
+};
 
-app.use("/", (req, res, next) => {
-  console.log("Done");
+app.use(cors(corsOptions));
+
+// Move the root route handler to the end to avoid it overriding other routes
+app.use("/users", userRoutes);
+
+app.use("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
-
-app.use("/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
