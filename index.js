d210b125161bd9ca7ca6f6fb0640cc22311e8995
navnee8989 <sondarvanavneet444@gmail.com>
2024-05-18 17:17:18 +0530

    changes

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const userRoutes = require('./routes/Authroutes');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-
+const express = require("express");
+const userRoutes = require("./routes/Authroutes");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config();
 
 const app = express();
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use("/", (req, res, next) => {
+  console.log("Done");
+  res.send("<h1>Hello World</h1>");
+});
 
-app.use('/',()=>{
-    console.log("DOne");
-}
-
-)
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
